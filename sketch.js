d018e1b5ada69e80a46f083ce14c00d11e1a2762
RudraPratapRudra <75071229+RudraPratapRudra@users.noreply.github.com>
2021-01-26 23:09:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+
+  detectollision(stoneObj,mango1);
+  detectollision(stoneObj,mango2);
+  detectollision(stoneObj,mango3);
+  detectollision(stoneObj,mango4);
+  detectollision(stoneObj,mango5);
+  
+
+  treeObj.display();
+  mango1.display();
+
+  groundObject.display();
+}
+function kyePressed(){
+if(keyCode===32){
+Matter.Body.setPosition(stoneObj.body,{x:235,y:420})	
+launcherObject.attach(stoneObj.body);
+
+ }	
+}
+function detectollision(Lstone,Lmango){
+mangoBodyPosition=lmango.body.position	
+stoneBodyPosition=lstone.body.position
+
+var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+if(distance<=lmango.r+lstone.r)
+{
+	Matter.Body.setStatic(lmango.body,false);
+}
+
+}
